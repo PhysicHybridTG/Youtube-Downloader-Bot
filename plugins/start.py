f30270ea5620ee65e0b8cfb17b20d6b6ea072915
Physic_Hybrid <85246569+PhysicHybridTG@users.noreply.github.com>
2021-06-09 17:28:58 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -8,9 +8,12 @@ async def start(client, message):
         [InlineKeyboardButton("Support Group", url="https://t.me/InFoTel_Group")],
         [InlineKeyboardButton(
             "My Father 👩‍💻", url="https://t.me/Physic_hybrid")]
-        [InlineKeyboardButton(
-            "Help 🙋", url="https://t.me/InFoTel_Group")]
+        
     ])
-    welcomed = f"Hey <b>{message.from_user.first_name}</b>\n/help for More info"
+    welcomed = f"Hey <b>{message.from_user.first_name}</b>\n/
+    
+Am YouTube Downloader Bot 😎
+
+I will convert YouTube link to Video / File & MP3.."
     await message.reply_text(welcomed, reply_markup=joinButton)
     raise StopPropagation
