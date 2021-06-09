ddda44f0e9b4d76a66a4b630cf231484a1b1a7a3
Physic_Hybrid <85246569+PhysicHybridTG@users.noreply.github.com>
2021-06-09 12:09:51 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -7,7 +7,7 @@ async def start(client, message):
     joinButton = InlineKeyboardMarkup([
         [InlineKeyboardButton("Support Group", url="https://t.me/InFoTel_Group")],
         [InlineKeyboardButton(
-            "My Father 👩‍💻", https://t.me/Physic_hybrid")]
+            "My Father 👩‍💻", url="https://t.me/Physic_hybrid")]
     ])
     welcomed = f"Hey <b>{message.from_user.first_name}</b>\n/help for More info"
     await message.reply_text(welcomed, reply_markup=joinButton)
