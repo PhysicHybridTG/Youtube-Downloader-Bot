ce2489ca21ca580f75c3130dbe72d4b08bc48e15
Physic_Hybrid <85246569+PhysicHybridTG@users.noreply.github.com>
2021-06-10 14:31:01 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -1,15 +1,89 @@
-from pyrogram import Client, Filters, StopPropagation, InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram import CLient, Filters
+from pyrogram import InlineKeyboardButton, InlineKeyboardMarkup
 
 
-@Client.on_message(Filters.command(["start"]), group=-2)
-async def start(client, message):
-    # return
-    joinButton = InlineKeyboardMarkup([
-        [InlineKeyboardButton("Support Group", url="https://t.me/InFoTel_Group")],
-        [InlineKeyboardButton(
-            "My Father 👩‍💻", url="https://t.me/Physic_hybrid")]
-        
-    ])
-    welcomed = f"Hey <b>{message.from_user.first_name}</b>\n/help for More info"
-    await message.reply_text(welcomed, reply_markup=joinButton)
-    raise StopPropagation
+
+
+@Client.on_message(Filters.command(["start"]), 1)
+async def start (bot, update):
+
+    await bot.send_message(
+        chat_id=update.chat.id,
+        text="start msg",
+        parse_mode="html",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    
+                    InlineKeyboardButton("Dev", url="https://t.me/username"),
+                    InlineKeyboardButton("Channel", url="https://t.me/username")
+                ],
+                [
+                    InlineKeyboardButton("Help", callback="help_menu"),
+                    InlineKeyboardButton("Close", callback="close_btn")
+                ]
+            ]
+        )
+    )
+
+
+
+@Client.on_message(Filters.command(["about"]), 1)
+async def about(bot, update):
+
+    await bot.send_message(
+        chat_id=update.chat.id,
+        text="about msg",
+        parse_mode="html",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton("Close", callback="close_btn")
+                ]
+            ]
+        )
+    )
+
+
+
+
+@Client.on_callback_query(Filters.regex(r"help_menu"), 2)
+async def help_cb(bot, update):
+      
+      await update.message.edit_text(
+           text="help text",
+           parse_mode="html",
+           reply_markup=InlineKeyboardMarkup(
+              [
+                  [
+                      InlineKeyboardButton("About", callback="about_menu"),
+                      InlineKeyboardButton("Close", callback="close_btn")
+                  ]
+              ]
+            )
+
+      )
+
+
+
+@Client.on_callback_query(Filters.regex(r"about_menu"), 2)
+async def about_cb(bot, update):
+      
+      await update.message.edit_text(
+           text="about text",
+           parse_mode="html",
+           reply_markup=InlineKeyboardMarkup(
+              [
+                  [
+                      InlineKeyboardButton("Close", callback="close_btn")
+                  ]
+              ]
+            )
+
+      )
+
+
+@Client.on_callback_query(Filters.regex(r"close_btn"), 2)
+async def close_btn_cb(bot, update):
+      
+      await update.message.delete()
