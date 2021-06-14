90331d797dcd62430877e57e75401322ebaed5c4
Physic_Hybrid <85246569+PhysicHybridTG@users.noreply.github.com>
2021-06-14 10:10:19 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -98,7 +98,7 @@ async def about_cb(bot, update):
 <b>○ Server :</b> <a href="https://herokuapp.com/">Heroku</a>
 <b>○ Database :</b> <a href="https://www.mongodb.com/">MongoDB</a>
 <b>○ Build Status :</b> <code>V2.1 [BETA]</code>
-<b>📜 Quote :</b> <code>ആരും പേടിക്കണ്ട എല്ലാവർക്കും കിട്ടും™️</code>"""
+<b>📜 Quote :</b> <code>ആരും പേടിക്കണ്ട എല്ലാവർക്കും കിട്ടും™️</code>""".format(update.from_user.mention)
 
            parse_mode="html",
            reply_markup=InlineKeyboardMarkup(
