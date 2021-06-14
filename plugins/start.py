240e96c8973da0b6d0e4d3673ce4dcf037ef260a
Physic_Hybrid <85246569+PhysicHybridTG@users.noreply.github.com>
2021-06-14 12:09:11 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -67,10 +67,10 @@ async def about(bot, update):
 async def help_cb(bot, update):
       
       await update.message.edit_text(
-           text=f"Hey 🙋‍♂️{update.from_user.mention} Please Follow This step
+           text="""🙋🏻‍♂️   Hey    <code> {}🤓</code> Please Follow This step
 <b>○ Just Send Youtube Url
 <b>○ Only Youtube Url Support
-<b>○ ⚠️Playlist Note support",
+<b>○ ⚠️Playlist Note support""",
            parse_mode="html",
            reply_markup=InlineKeyboardMarkup(
               [
