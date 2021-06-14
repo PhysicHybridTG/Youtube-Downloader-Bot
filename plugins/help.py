f9788338d1161f31440f77f5f7e0087ca865e524
Physic_Hybrid <85246569+PhysicHybridTG@users.noreply.github.com>
2021-06-14 15:12:53 +0530

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -3,5 +3,5 @@ from pyrogram import Client, Filters
 
 @Client.on_message(Filters.command(["help"]))
 async def start(client, message):
-    helptxt = f"<code>Click The Help Button And See The Magic</code>"
+    helptxt = f"<code>Click The Help Button And See The Magic..🤡</code>"
     await message.reply_text(helptxt)
